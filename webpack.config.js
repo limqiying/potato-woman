43fe7fa0189024b9f3fc278613d1925207836b21
Tia <limqiying19@gmail.com>
2019-10-30 19:45:50 -0400

    init template

---
+++ webpack.config.js
@@ -0,0 +1,17 @@
+const path = require('path');
+
+module.exports = {
+    entry: './src/index.js',
+    output: {
+        filename: 'main.js',
+        path: path.resolve(__dirname, 'dist'),
+    },
+};
+
+module: {
+    rules: [{
+        test: /\.(js)$/,
+        exclude: /node_modules/,
+        use: ["eslint-loader"]
+    }]
+}
